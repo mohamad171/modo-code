8e20ebaa2fae675615da1d5f19b2a3cba7a6e0f9
mohamad171 <mohamadmohamadi249@gmail.com>
2025-02-25 16:26:58 +0330

    celery

--- modo_code/settings.py
+++ modo_code/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+CELERY_BROKER_URL = 'redis://redis:6379/4'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
