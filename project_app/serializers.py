ffe68bee10bac11f7ba63af68b560f841c0e481f
mohamad171 <mohamadmohamadi249@gmail.com>
2025-02-27 14:08:06 +0330

    user as read only

--- project_app/serializers.py
+++ project_app/serializers.py
@@ -7,6 +7,7 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = '__all__'
+        read_only_fields = ["user"]
 
 
 class SaveNodeAndRelationshipsSerializer(serializers.Serializer):
