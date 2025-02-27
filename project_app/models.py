3ae8c15d71191bf81a16419ec26952ad5fd44fab
mohamad171 <mohamadmohamadi249@gmail.com>
2025-02-27 14:09:13 +0330

    nullable description

--- project_app/models.py
+++ project_app/models.py
@@ -8,7 +8,7 @@ class Project(models.Model):
     local_path = models.CharField(max_length=300,null=True,blank=True)
     name = models.CharField(max_length=100)
     language = models.CharField(max_length=50,null=True,blank=True)
-    description = models.TextField()
+    description = models.TextField(null=True,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
