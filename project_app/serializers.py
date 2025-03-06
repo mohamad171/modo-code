a675e0ae9649a564039ac3ced59354f7de916194
mohamad171 <mohamadmohamadi249@gmail.com>
2025-03-06 21:07:01 +0330

    increase chunk size

--- project_app/serializers.py
+++ project_app/serializers.py
@@ -48,7 +48,7 @@ class SaveNodeAndRelationshipsSerializer(serializers.Serializer):
         # TODO this task should run on celery | add status to project model
         result = get_nodes_for_embedding(neo_db)
         text_data = []
-        chunk_size = 10
+        chunk_size = 50
         chunked_embeddings = []
         for i in result:
             text_data.append(i["text"])
