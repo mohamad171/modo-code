f26ce2615c9ffb7c5f7532226f1696c64bbb1382
mohamad171 <mohamadmohamadi249@gmail.com>
2025-03-13 13:18:11 +0330

    task admin

--- project_app/admin.py
+++ project_app/admin.py
@@ -6,3 +6,8 @@ from .models import *
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ["name","user","created_at","updated_at"]
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["project","task_type","task_status","created_at","updated_at"]
