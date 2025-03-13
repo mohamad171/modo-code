db414a88ff5f692ac732e57d7c0d05e1f86cc346
mohamad171 <mohamadmohamadi249@gmail.com>
2025-03-13 13:24:07 +0330

    debug

--- project_app/tasks.py
+++ project_app/tasks.py
@@ -19,46 +19,46 @@ def run_task(self,**kwargs):
         task.task_status = Task.TaskStatusChoices.DOING
         task.save()
 
-        try:
-            nodes = kwargs["nodes"]
-            relationships = kwargs["relationships"]
+        # try:
+        nodes = kwargs["nodes"]
+        relationships = kwargs["relationships"]
 
-            nodes = json.loads(nodes)
-            relationships = json.loads(relationships)
-            neo_db = Neo4jManager(repoId=str(project.id), entityId=str(project.id))
-            neo_db.save_graph(nodes, relationships)
-            neo_db.close()
+        nodes = json.loads(nodes)
+        relationships = json.loads(relationships)
+        neo_db = Neo4jManager(repoId=str(project.id), entityId=str(project.id))
+        neo_db.save_graph(nodes, relationships)
+        neo_db.close()
 
-            result = get_nodes_for_embedding(neo_db)
-            text_data = []
-            chunk_size = 100
-            chunked_embeddings = []
-            for i in result:
-                text_data.append(i["text"])
+        result = get_nodes_for_embedding(neo_db)
+        text_data = []
+        chunk_size = 100
+        chunked_embeddings = []
+        for i in result:
+            text_data.append(i["text"])
 
-            for j in range(0, len(text_data), chunk_size):
-                chunk = text_data[j:j + chunk_size]
-                for text in chunk:
-                    embedding = Embedding(account_id=os.getenv("CLOUDFLARE_ACCOUNT_ID"),
-                                          api_token=os.getenv("CLOUDFLARE_API_TOKEN")).embedded(text)
-                    chunked_embeddings.append(embedding)
-                    time.sleep(0.3)
+        for j in range(0, len(text_data), chunk_size):
+            chunk = text_data[j:j + chunk_size]
+            for text in chunk:
+                embedding = Embedding(account_id=os.getenv("CLOUDFLARE_ACCOUNT_ID"),
+                                      api_token=os.getenv("CLOUDFLARE_API_TOKEN")).embedded(text)
+                chunked_embeddings.append(embedding)
+                time.sleep(0.3)
 
-            ids = [item["node_id"] for item in result]
-            chroma = ChromaManager(project.id)
+        ids = [item["node_id"] for item in result]
+        chroma = ChromaManager(project.id)
 
-            for j in range(0, len(text_data), chunk_size):
-                chunk_text = text_data[j:j + chunk_size]
-                chunk_ids = ids[j:j + chunk_size]
-                chunk_embeddings = chunked_embeddings[j:j + chunk_size]
+        for j in range(0, len(text_data), chunk_size):
+            chunk_text = text_data[j:j + chunk_size]
+            chunk_ids = ids[j:j + chunk_size]
+            chunk_embeddings = chunked_embeddings[j:j + chunk_size]
 
-                chroma.save_graph(chunk_text, chunk_embeddings, chunk_ids)
-            task.task_status = Task.TaskStatusChoices.DONE
-            task.save()
-        except Exception as ex:
-            print(ex)
-            task.task_status = Task.TaskStatusChoices.ERROR
-            task.error_text = str(ex)
-            task.save()
+            chroma.save_graph(chunk_text, chunk_embeddings, chunk_ids)
+        task.task_status = Task.TaskStatusChoices.DONE
+        task.save()
+        # except Exception as ex:
+        #     print(e)
+        #     task.task_status = Task.TaskStatusChoices.ERROR
+        #     task.error_text = str(ex)
+        #     task.save()
 
 
